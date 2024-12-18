9d208be72c76f4c97d3e09ec181853028a270851
kodongwon <dw9706@naver.com>
2024-12-18 21:18:32 +0900

    fix: application eureka

--- src/main/java/com/simsimbookstore/apiserver/ApiServerApplication.java
+++ src/main/java/com/simsimbookstore/apiserver/ApiServerApplication.java
@@ -1,13 +1,21 @@
 package com.simsimbookstore.apiserver;
 
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.EurekaClient;
+import jakarta.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiServerApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(ApiServerApplication.class, args);
     }
+
+
 
 }
